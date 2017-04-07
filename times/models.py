4f302cb8151c11c3de85ab54744a951e011eb7bc
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-04-07 15:53:56 +0800

    Got a basic view detailed view going for a timesheet entry

--- times/models.py
+++ times/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from datetime import timedelta
 
 class Time(models.Model):
     start = models.DateTimeField('Start Time')
     end = models.DateTimeField('End Time', null=True)
+    lunch = models.IntegerField('Lunch', default=0)
+
     morning_task = models.TextField(null=True)
     afternoon_task = models.TextField(null=True)
 
     def __str__(self):
         return 'Timesheet Entry: {}'.format(self.start.strftime('%x'))
+
+    def hours_worked(self):
+        duration = self.end - self.start - timedelta(minutes=self.lunch)
+        return duration.total_seconds() / 3600
+
+
