08c08b0b1a2986d7666cb7162334eb32ed066d04
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-04-07 16:24:31 +0800

    Got basic auth and a home page

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    url(r'^times/', include('times.urls')),
+    url(r'^times/', include('times.urls', namespace='times')),
+    url(r'^login/$', auth_views.login, {'template_name': 'home/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
-    url(r'', include('home.urls')),
+    url(r'', include('home.urls', namespace='home')),
 ]
