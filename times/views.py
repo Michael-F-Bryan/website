3fb49f485314b6d83dba5be65c4836f535e4fc13
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-04-07 16:54:31 +0800

    Gave the list_all view a pretty face

--- times/views.py
+++ times/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 from .models import Time
 
+@login_required
 def detail(request, time_id):
     time = get_object_or_404(Time, pk=time_id)
     return render(request, 'times/detail.html', {'time': time})
 
+@login_required
 def list_all(request):
     times = Time.objects.all()
     return render(request, 'times/list_all.html', {'times': times})
