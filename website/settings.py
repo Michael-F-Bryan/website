ed7b3932b407da0a70f22a255afa73b35a6a4fb8
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-05-01 02:46:52 +0800

    Removed the production_file and debug_file loggers

--- website/settings.py
+++ website/settings.py
@@ -165,27 +165,8 @@ LOGGING = {
         },
         'console': {
             'level': 'DEBUG',
-            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
             'formatter': 'main_formatter',
-        },
-        'production_file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': '/var/log/website/main.log',
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 7,
-            'formatter': 'main_formatter',
-            'filters': ['require_debug_false'],
-        },
-        'debug_file': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug_website.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 7,
-            'formatter': 'main_formatter',
-            'filters': ['require_debug_true'],
         },
         'null': {
             "class": 'logging.NullHandler',
@@ -204,9 +185,8 @@ LOGGING = {
             'handlers': ['null'],
         },
         '': {
-            'handlers': ['console', 'production_file', 'debug_file'],
+            'handlers': ['console'],
             'level': "DEBUG",
-            'propagate': True,
         },
     }
 }
