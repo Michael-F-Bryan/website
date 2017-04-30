790323e474f86b64570183c99b3effa7f918b934
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-05-01 01:27:04 +0800

    Added time_slices to dropdown menu and reordered time urls

--- times/urls.py
+++ times/urls.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<time_id>\d+)$', views.detail, name='detail'),
-    url(r'^delete/(?P<time_id>\d+)$', views.delete, name='delete'),
     url(r'^new$', login_required(views.NewTime.as_view()), name='new'),
+    url(r'^(?P<time_id>\d+)/delete$', views.delete, name='delete'),
+    url(r'^(?P<time_id>\d+)/edit$', login_required(views.TimeEdit.as_view()), name='edit'),
+    url(r'^(?P<time_id>\d+)$', views.detail, name='detail'),
     url(r'^time-slice/(?P<hash>[\d\w]+)$', views.slice, name='slice'),
     url(r'^time-slice/new$', login_required(views.CreateTimeSlice.as_view()), name='new_time_slice'),
     url(r'^time-slice$', views.all_time_slices, name='time_slices'),
     url(r'^download$', views.download_as_csv, name='download'),
-    url(r'^edit/(?P<time_id>\d+)$', login_required(views.TimeEdit.as_view()), name='edit'),
     url(r'^$', views.list_all, name='list_all'),
 ]
