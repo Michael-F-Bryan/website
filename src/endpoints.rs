5b87d6ff30fcb32f9acaa3a612f4c5380a4604e1
Michael Bryan <michaelfbryan@gmail.com>
2017-10-02 11:32:40 +0800

    Added caching for static assets and a navbar

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,13 +1,15 @@
 #![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value, double_parens))]
 
+use std::time::Duration;
+use std::path::{Path, PathBuf};
 use rocket::{Config, Rocket};
 use rocket::config::Environment;
-use rocket::http::Cookies;
+use rocket::http::{Cookies, Status};
 use rocket::request::Form;
-use rocket::response::{NamedFile, Redirect};
+use rocket::request::Request;
+use rocket::response::{NamedFile, Redirect, Responder, Response};
 use rocket_contrib::Template;
 use dotenv;
-use std::path::{Path, PathBuf};
 
 use sessions::{Session, SessionManager};
 use db::{self, DbConn};
@@ -81,6 +83,36 @@ fn login() -> Template {
 }
 
 #[get("/static/<file..>")]
-fn static_files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
+fn static_files(file: PathBuf) -> Option<Cached<NamedFile>> {
+    NamedFile::open(Path::new("static/").join(file))
+        .map(|f| Cached::new(Duration::from_secs(3600), f))
+        .ok()
+}
+
+#[derive(Debug)]
+pub struct Cached<T> {
+    seconds: u32,
+    inner: T,
+}
+
+impl<T> Cached<T> {
+    pub fn new(duration: Duration, inner: T) -> Cached<T> {
+        let seconds = duration.as_secs() as u32;
+
+        Cached { seconds, inner }
+    }
+}
+
+impl<'a, T> Responder<'a> for Cached<T>
+where
+    T: Responder<'a>,
+{
+    fn respond_to(self, request: &Request) -> ::std::result::Result<Response<'a>, Status> {
+        use rocket::http::hyper::header::{CacheControl, CacheDirective};
+
+        Response::build()
+            .merge(self.inner.respond_to(request)?)
+            .header(CacheControl(vec![CacheDirective::MaxAge(self.seconds)]))
+            .ok()
+    }
 }
