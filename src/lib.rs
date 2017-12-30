76adfc04a521d2e1b4cf28c781a4d760a0ee53cb
Michael Bryan <michaelfbryan@gmail.com>
2017-12-31 00:49:20 +0800

    Stubbed out a couple templates

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,38 @@
 #![feature(plugin)]
+#![feature(use_extern_macros)]
 #![plugin(rocket_codegen)]
 
+extern crate log;
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
-use rocket::Rocket;
+use std::path::{Path, PathBuf};
+use rocket::{Request, Rocket};
+use rocket::response::NamedFile;
+use rocket_contrib::Template;
 
 /// Create the web server.
 pub fn create_server() -> Rocket {
     Rocket::ignite()
+        .catch(errors![not_found])
+        .mount("/", routes![home])
+        .attach(Template::fairing())
+}
+
+#[error(404)]
+fn not_found(_: &Request) -> Template {
+    let context = json!{{}};
+    Template::render("not_found", context)
+}
+
+#[get("/static/<file..>")]
+fn static_assets(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[get("/")]
+fn home() -> Template {
+    let context = json!{{}};
+    Template::render("home", context)
 }
