7fc54e63237c5312c74e5be649279ed57e777853
Michael Bryan <michaelfbryan@gmail.com>
2019-04-14 20:37:24 +0800

    Added a thing for displaying the markdown description

--- frontend/src/views/ViewTime.vue
+++ frontend/src/views/ViewTime.vue
@@ -12,7 +12,10 @@
                     :title="entry.start.format('LLLL')"
                 >{{entry.start.format('LT')}}</dd>
                 <dt class="col-md-3">End</dt>
-                <dd class="col-md-9" :title="entry.end.format('LLLL')">{{entry.end.format('LT')}}</dd>
+                <dd
+                    class="col-md-9"
+                    :title="entry.end ? entry.end.format('LLLL') : ''"
+                >{{entry.end ? entry.end.format('LT') : '-'}}</dd>
                 <dt class="col-md-3">Breaks</dt>
                 <dd
                     class="col-md-9"
@@ -21,12 +24,20 @@
                 <dd class="col-md-9">{{Math.round(entry.timeWorked.asHours()*10)/10}} hrs</dd>
             </dl>
         </b-card>
+
+        <b-row class="mt-md-4" v-if="entry.description">
+            <h3>Description</h3>
+        </b-row>
+        <b-row v-if="entry.description">
+            <div v-html="description"></div>
+        </b-row>
     </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Entry from '@/client/Entry';
+import marked from 'marked';
 
 @Component({})
 export default class ViewTime extends Vue {
@@ -35,6 +46,10 @@ export default class ViewTime extends Vue {
     public mounted() {
         const id = this.$route.params.id;
         this.entry = this.$store.state.times[id];
+    }
+
+    get description(): string {
+        return marked(this.entry.description);
     }
 }
 </script>
